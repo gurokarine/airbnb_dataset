c5418c27324c44fcf12b7d782e6cdcb17894c741
Guro <guro.7@hotmail.com>
2017-03-11 12:10:48 +0100

    task 3d, needs clean up and review

---
+++ src/task3d.scala
@@ -0,0 +1,18 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object task3d {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("AirBnB").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val file2 = sc.textFile("..\\airbnb_data\\listings_us.csv")
+
+    val listingsData = file2.map(line => line.split("\t")).mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
+
+    val cities = listingsData.map(row => (row(15), ((0+row(80)).toDouble, 1)))
+    val nights_per_year = cities.reduceByKey((a,b) => (a._1 + b._1 , a._2 + b._2))
+
+    nights_per_year.foreach(row => println((row._1,row._2._1*1.3*3*12)))
+
+  }
+}
