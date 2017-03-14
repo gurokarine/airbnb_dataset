68f6e732e9da4ca172e7a60346f312bb2c6e4bf4
balder <balderrh_94@hotmail.com>
2017-03-14 10:36:22 +0100

    task5 finished

--- src/task5b.scala
+++ src/task5b.scala
@@ -1,6 +1,6 @@
 import org.apache.spark.{SparkConf, SparkContext}
 
-object task5a {
+object task5b {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("AirBnB").setMaster("local[*]")
     val sc = new SparkContext(conf)
@@ -11,14 +11,10 @@ object task5a {
     val listingsData = listings_file.map(line => line.split("\t")).mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
 
     val reviews = reviewsData.map(row => (row(0), (row(3), row(4))))
-    val cities = listingsData.map(row => (row(43), row(15)))
+    val cities = listingsData.map(row => (row(43), row(65).replaceAll("[$,]", "").toDouble))
 
-    val city_reviews = cities.join(reviews).map(row => (row._2._1 +" - "+ row._2._2._1+" - "+row._2._2._2, 1))
+    val city_reviews = cities.join(reviews).map(row => (row._2._2._1+" - "+row._2._2._2, row._2._1))
     val best_reviewers = city_reviews.reduceByKey((a,b) => a + b)
-    best_reviewers.filter(_._1.contains("New York")).map(_.swap).top(3).foreach(println)
-    best_reviewers.filter(_._1.contains("Seattle")).map(_.swap).top(3).foreach(println)
-    best_reviewers.filter(_._1.contains("San Francisco")).map(_.swap).top(3).foreach(println)
-
-
+    best_reviewers.map(_.swap).top(100).foreach(println)
   }
 }
