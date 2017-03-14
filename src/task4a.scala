85ddc724f2f22ec4e62a370f9f4160300a7ea32e
Guro <guro.7@hotmail.com>
2017-03-14 12:57:33 +0100

    optimized and completed

--- src/task4a.scala
+++ src/task4a.scala
@@ -9,11 +9,14 @@ object task4a {
 
     val listingsData = file2.map(line => line.split("\t")).mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
 
-    val cities = listingsData.map(row => (0+row(39)).toDouble).count()
-    val num_hosts = listingsData.map(row => row(28).toLong).distinct().count()
-    println("Number of listings: " + cities)
-    println("Number of hosts: " + num_hosts)
-    println("Average: " + cities/num_hosts.toDouble)
+    val num_listings = listingsData.map(row => (row(28).toLong, (0+row(31)).toDouble))
+    val num_listings_reduce = num_listings.reduceByKey((a,b) => a)
 
+    val num_hosts = num_listings_reduce.count()
+    val num_listings_count = num_listings.count()
+
+    println("Number of listings: " + num_listings_count)
+    println("Number of hosts: " + num_hosts)
+    println("Average: " + (num_listings_count/num_hosts.toDouble))
   }
 }
